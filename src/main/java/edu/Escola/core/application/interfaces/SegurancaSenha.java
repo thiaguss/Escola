5b89cc41c49393013be858d69005935fc2ff64de
Thiaguss <tsantos7098@gmail.com>
2024-06-19 20:05:47 -0300

    Adicionado o atributo senha ao aluno

---
+++ src/main/java/edu/Escola/core/application/interfaces/SegurancaSenha.java
@@ -0,0 +1,2 @@
+package edu.Escola.core.application.interfaces;public interface SegurancaSenha {
+}
--- src/main/java/edu/Escola/core/domain/entities/Aluno.java
+++ src/main/java/edu/Escola/core/domain/entities/Aluno.java
@@ -12,6 +12,7 @@ public class Aluno {
     private Cpf cpf;
     private String nome;
     private Email email;
+    private String senha;
     private List<Telefone> telefones = new ArrayList<>();
 
     public Aluno(Cpf cpf, String nome, Email email) {
